f9981012d61369e86640bb9b6594dec3feb4c9a6
yaroslav2k <iaroslav2k@gmail.com>
2023-04-26 01:58:06 +0300

    Implement tokenizer normalization

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -3,14 +3,40 @@ import json
 import io
 import tokenize
 
+def substitue_function_invokations(tokens):
+   prev_token = None
+   is_function: bool = False
+   function_index: int = -1
+
+   for i, token in enumerate(tokens):
+      if token == "(" and prev_token == "NAME":
+         is_function = True
+         function_index = i - 1
+      elif token == ")" and is_function:
+         print("DETECTED FN", prev_token)
+         is_function = False
+         tokens[function_index] = "FNINVOK"
+
+      prev_token = token
+
+def filter_token(token: str) -> str:
+   return token and len(token.strip()) > 0
+
+def transform_token(token_info: tokenize.TokenInfo) -> str:
+   if token_info[0] == tokenize.NAME:
+     return tokenize.tok_name[token_info[0]]
+
+   return token_info[1]
 
 def extract_tokens(source: str):
-   fn_filter = lambda item: item and len(item) > 0
-   fn_mapper = lambda item: str(item[1]).strip()
+   impl = lambda item: transform_token(item)
+   gen = tokenize.generate_tokens(io.StringIO(source).readline)
 
-   return list(
-      filter(fn_filter, map(fn_mapper, tokenize.generate_tokens(io.StringIO(source).readline)))
-   )
+   tokens = list(filter(filter_token, map(transform_token, gen)))
+   substitue_function_invokations(tokens)
+
+
+   return tokens
 
 
 def main():
@@ -28,9 +54,9 @@ def main():
 
       for datum in data:
          tokens_spec = extract_tokens(datum["item"])
-         result.append(json.dumps({ "tokens": tokens_spec, "identifier": datum["identifier"] }))
+         result.append({ "tokens": tokens_spec, "identifier": datum["identifier"] })
 
-      print(result)
+      print(json.dumps(result))
 
 
 if __name__ == "__main__":
