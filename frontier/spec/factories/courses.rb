a55cb4098a4612a10607a5eb822376da893dc1c5
yaroslav2k <iaroslav2k@gmail.com>
2023-10-20 00:03:11 +0300

    Fix specs

--- frontier/spec/factories/courses.rb
+++ frontier/spec/factories/courses.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :uuid             not null, primary key
+#  semester   :string           not null
+#  title      :string           not null
+#  year       :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid
+#
+# Indexes
+#
+#  index_courses_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :course do
     semester { %w[fall spring].sample }
