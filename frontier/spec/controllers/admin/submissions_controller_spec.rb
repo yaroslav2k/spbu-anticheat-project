e319721752c38383442a40105c8c9f5042dc93df
viralpraxis <iaroslav2k@gmail.com>
2023-11-15 23:31:10 +0300

    Update `frontier` specs

---
+++ frontier/spec/controllers/admin/submissions_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe Admin::SubmissionsController do
+  describe "#index" do
+    render_views
+
+    let(:user) { create(:user) }
+
+    before { sign_in(user) }
+
+    specify do
+      get :index
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
