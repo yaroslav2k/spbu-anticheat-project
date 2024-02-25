6f33674a4c459b643b5311e87e6437dbb8217544
viralpraxis <iaroslav2k@gmail.com>
2024-02-25 21:10:54 +0300

    Fix `production` mode `i18n` fallback options

--- frontier/config/environments/production.rb
+++ frontier/config/environments/production.rb
@@ -53,10 +53,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
