ea057842a9ff46ded76c7822d200201564af87f3
viralpraxis <iaroslav2k@gmail.com>
2024-04-13 21:52:31 +0300

    Mistral client enhancements

--- detector/src/main/java/ru/spbu/detector/mistral/Mistral.java
+++ detector/src/main/java/ru/spbu/detector/mistral/Mistral.java
@@ -1,22 +1,31 @@
 package ru.spbu.detector.mistral;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import reactor.core.publisher.Mono;
+import java.util.List;
+
 import ru.spbu.detector.mistral.builder.MessageListBuilder;
 import ru.spbu.detector.mistral.client.MistralClient;
 import ru.spbu.detector.mistral.completion.ChatCompletionRequest;
 import ru.spbu.detector.mistral.completion.ChatCompletionResponse;
 import ru.spbu.detector.mistral.completion.Message;
 
-import java.util.List;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import reactor.core.publisher.Mono;
 
 public class Mistral {
+    private static final String PROMPT_INITIAL_MESSAGE =
+      """
+      Compare these two implementations of the same function and answer \
+      whether there are signs of copying in these works. Consider the length of the entire text, \
+      but do not take into account the names of the functions. On the first line of the answer write Yes or No, then on the next line, \
+      if there are signs of copying, write your comment.
+      """;
+
     public static String compareTwo(String prg1, String prg2) {
         MistralClient client = new MistralClient();
 
         String model = "open-mistral-7b";
         List<Message> messages = new MessageListBuilder()
-                .system("Сравни эти две реализации одной функции и ответь, есть ли признаки копирования в этих работах. Учитвай длину всего текста, но не учитывай названия функций. На первой строке ответа напиши Да или Нет, далее, на следующей строке, если присутствуют признаки копирования, напиши свой комментарий")
+                .system(PROMPT_INITIAL_MESSAGE)
                 .user(prg1 + "/n/n" + prg2)
                 .build();
 
@@ -36,4 +45,6 @@ public class Mistral {
         Message firstChoice = response.block().getChoices().get(0).getMessage();
         return firstChoice.getRole() + ":\n" + firstChoice.getContent() + "\n";
     }
-}
+
+    private Mistral() {}
+  }
