f601ff60e837a29eac5dd8d2107031ef77a898be
viralpraxis <iaroslav2k@gmail.com>
2024-05-23 07:26:53 +0300

    Tweak assignment decorator

--- frontier/app/decorators/assignment_decorator.rb
+++ frontier/app/decorators/assignment_decorator.rb
@@ -4,7 +4,7 @@ class AssignmentDecorator < ApplicationDecorator
   include Memery
 
   CodeClone = Data.define(:similarity, :code_fragments) do
-    def initialize(similarity:, code_fragments:)
+    def initialize(similarity:, code_fragments:) # rubocop:disable Metrics/PerceivedComplexity
       unless similarity.is_a?(Numeric) && (0..100).cover?(similarity)
         raise ArgumentError, <<~HEREDOC
           Expected `similarity` to be a floating-point number within 0..1 range.
@@ -16,6 +16,15 @@ class AssignmentDecorator < ApplicationDecorator
           Expected `code_fragments` to be an instance of array with
           elements of type `CodeFragment`.
         HEREDOC
+      end
+
+      grouped_code_fragments = code_fragments.group_by(&:author_name)
+      grouped_code_fragments.each_value do |student_code_fragments|
+        if student_code_fragments.size > 1 && grouped_code_fragments.keys > 1
+          (0...student_code_fragments.size - 1).each { code_fragments.delete(code_fragments[_1]) }
+        elsif student_code_fragments > 1 && grouped_code_fragments == 1
+          code_fragments = []
+        end
       end
 
       super(similarity: similarity.to_d / 100, code_fragments:)
